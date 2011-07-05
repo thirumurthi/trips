f579fc1f86a745f0018e6a72616b4a9c1dc473ef
thirumurthi <thiru@onederr.com>
2011-07-05 15:16:28 +0530

    config

---
+++ config/application.php
@@ -0,0 +1,80 @@
+<?php
+
+$config['modules'] = array ( 
+	MODPATH.'updates',
+	MODPATH.'answers',
+	MODPATH.'papers',
+	MODPATH.'blog',
+	MODPATH.'friends',
+	MODPATH.'classifieds',
+	MODPATH.'events',
+	MODPATH.'forum',
+	MODPATH.'groups',
+	MODPATH.'inbox',
+	MODPATH.'news',
+	MODPATH.'photos',
+	MODPATH.'video',
+	MODPATH.'ktwitter',
+	MODPATH.'lib',
+	MODPATH.'debug_toolbar',
+	MODPATH.'gmaps',
+	MODPATH.'admin',
+	MODPATH.'profile',
+	MODPATH.'users',
+	MODPATH.'cms',
+	MODPATH.'bookmarks',
+	MODPATH.'crawl'
+);
+
+/** Home page Add **/
+$config['AD_4'] = <<<DEMO
+<script type="text/javascript"><!--
+google_ad_client = "pub-5409811093151759";
+/* 234x60, created 5/4/10 */
+google_ad_slot = "8227666281";
+google_ad_width = 234;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+DEMO;
+
+/** Home page Add **/
+$config['AD_3'] = <<<DEMO
+<script type="text/javascript"><!--
+google_ad_client = "pub-5409811093151759";
+/* 728x15, created 5/3/10 */
+google_ad_slot = "8028076297";
+google_ad_width = 728;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+DEMO;
+
+/** 468 x 10 **/
+$config['AD_2'] = <<<DEMO
+        <script type="text/javascript"><!--
+google_ad_client = "pub-5409811093151759";
+/* 468x15, created 5/2/10 */
+google_ad_slot = "7922962260";
+google_ad_width = 468;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+DEMO;
+
+/** Home page Add **/
+$config['AD_1'] = <<<DEMO
+<!-- ca-pub-5409811093151759/sawaal -->
+<script type='text/javascript'>
+GA_googleFillSlot("sawaal");
+</script>
+DEMO;
